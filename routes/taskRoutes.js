3320a9f8b45433731f29f5ae1618632efab984fb
Amina Iftikhar <144228946+AminaIftikharr@users.noreply.github.com>
2024-01-25 01:50:39 +0500

    updated files

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth');
 const Task = require('../models/Task');
+const { findOne, find } = require('../models/User');
+const { object } = require('webidl-conversions');
 
 router.get('/test', auth, (req,res)=> {
     res.json({
@@ -64,4 +66,55 @@ router.get('/:id', auth, async(req,res)=>{
         res.status(400).send({error: err});
     }
 })
+
+//update a task
+router.patch('/:id', auth, async(req,res)=>{
+    const taskid= req.params.id;
+    const updates= object.keys(req.body);
+    const allowedUpdates= ['description', 'completed'];
+    const isValidOperation = updates.every(update=>allowedUpdates.includes(update));
+    if(!isValidOperation){
+        return res.status(400).json({error: "invalid updates"});
+    }
+    try{
+        const task = await findOne({
+            _id: taskid,
+            owner: req.user._id
+        })
+        if(!task){
+            res.status(404).json({message: 'Task not found'});
+        }
+        updates.forEach(update =>task[update]= req.body[update]);
+        await task.save();
+
+        res.json({
+            message: "Task Updated successfully"
+        })
+    }
+    catch(err){
+        res.status(404).send({error: err});
+    }
+})
+
+//delete a task by id
+router.delete('/:id', auth, async(req,res)=>{
+    const taskid= req.params.id;
+    try{
+        const task = await Task.findOneAndDelete({
+            _id: taskid,
+            owner: req.user,_id
+        })
+        if(!task){
+            res.status(404).json({message: "Task not found"});
+        }
+        res.json({
+            message: "Task Deleted successfully"
+        })
+
+    }
+    catch(err){
+        res.status(404).json({error: err});
+    }
+})
+
 module.exports = router;
